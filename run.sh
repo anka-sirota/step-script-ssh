d18f37dedb5958b409c253112b157e7a2192ca54
Anna Sirota <dev.local@gmail.com>
2014-03-28 15:28:29 +0100

    adding wercker env variables to ssh command

--- run.sh
+++ run.sh
@@ -4,4 +4,12 @@ then
 fi
 
 echo "$WERCKER_SSH $WERCKER_SCRIPT_SSH_CODE"
-$WERCKER_SSH "$WERCKER_SCRIPT_SSH_CODE"
+
+ENV=''
+
+for f in "$VARS_TO_EXPORT"
+do
+    ENV+="$f=`$f` "
+echo $ENV
+done
+$WERCKER_SSH "$ENV $WERCKER_SCRIPT_SSH_CODE"
